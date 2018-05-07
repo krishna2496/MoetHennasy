7d4796a504f7707c7c5cd1059fc85f11d3beb5aa
Krishna Dhanani <krishna.dhanani@sparsh1.com>
2018-05-07 12:09:09 +0530

    master data api

---
+++ api/modules/v1/controllers/MasterDataController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace api\modules\v1\controllers;
+use Yii;
+use common\repository\MasterDataRepository;
+use common\helpers\CommonHelper;
+use yii\filters\AccessControl;
+
+class MasterDataController extends BaseApiController
+{
+    public $modelClass = 'common\models\Cities';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['access'] = [
+            'class' => AccessControl::className(),
+            'ruleConfig' => [
+                'class' => \common\components\AccessRule::className(),
+            ],
+            'rules' => [
+                [
+                    'actions' => ['masters'],
+                    'allow' => true,
+                    'roles' => ['&'],
+                ],
+                ],
+        ];
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['masters']);
+        return $actions;
+    }
+
+    public function actionMasters()
+    {
+        $masterDataRepository = new MasterDataRepository;
+        $returnData = $masterDataRepository->listing();
+        return $returnData;
+    }
+    }
+
+
--- common/repository/MasterDataRepository.php
+++ common/repository/MasterDataRepository.php
@@ -6,6 +6,10 @@ use common\helpers\CommonHelper;
 use common\models\Cities;
 use common\models\Countries;
 use common\models\Province;
+use common\models\User;
+use common\models\Markets;
+use common\models\MarketSegments;
+use common\models\MarketSegmentData;
 
 class MasterDataRepository extends Repository
 {
@@ -41,5 +45,55 @@ class MasterDataRepository extends Repository
         $data['provinces'] = $query->asArray()->all();
         $this->apiData = $data;
         return $this->response();
+    }
+    
+    public function market($data = array()){
+    $this->apiCode = 1;
+    $id=$data['id'];
+    $data=array();
+    
+    $marketData=Markets::find()->where(['id'=>$id])->asArray()->one();
+    $marketSegmentData=MarketSegmentData::find()->andWhere(['market_id'=>$id])->asArray()->all();
+    
+    foreach($marketSegmentData as $key=>$value){
+        $marketSegmentDataArry[]=$value['market_segment_id'];
+    }
+    $marketSegmentDataIds= implode(',',$marketSegmentDataArry);
+ 
+    $marketSegmentDetail=MarketSegments::find()->where("id IN($marketSegmentDataIds) ")->asArray()->all();
+    $data['market']=$marketData;
+    $data['market']['marketSegment']=$marketSegmentDetail;
+    $this->apiData = $data;
+    return $this->response();
+    }
+    
+     public function listing($data = array()) {
+        $this->apiCode = 1;
+        $returnData = array();
+        
+        $user = Yii::$app->user->identity;
+        $model = User::findOne($user->id);
+        $marketId= isset($model->market_id)? $model->market_id : 0;
+        $returnData['countries'] = $this->countries();
+        if( $returnData['countries']['status']['success'] == 1 ){
+            $returnData['countries']=$returnData['countries']['data']['countries'];
+        }
+       
+        $returnData['cities'] = $this->cities();
+        if( $returnData['cities']['status']['success'] == 1 ){
+            $returnData['cities']=$returnData['cities']['data']['cities'];
+        }
+        $returnData['provinces'] = $this->provinces();
+        if( $returnData['provinces']['status']['success'] == 1 ){
+            $returnData['provinces']=$returnData['provinces']['data']['provinces'];
+        }
+        
+        $returnData['market'] = $this->market(array('id'=>$marketId));
+        if( $returnData['market']['status']['success'] == 1 ){
+            $returnData['market']=$returnData['market']['data']['market'];
+        }
+        $this->apiData = $returnData;
+        return $this->response();
+       
     }
 }
