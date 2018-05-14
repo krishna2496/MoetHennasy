decb001f1f5f3bd0e610178785f22fd4ae1dec5d
Krishna Dhanani <krishna.dhanani@sparsh1.com>
2018-05-14 16:36:08 +0530

    USER ACTIVITY CODE ADDED

--- backend/controllers/BaseBackendController.php
+++ backend/controllers/BaseBackendController.php
@@ -6,8 +6,6 @@ use Yii;
 use yii\filters\AccessControl;
 use common\controllers\BaseController;
 use yii\web\BadRequestHttpException;
-use common\helpers\CommonHelper;
-use common\models\ActionLog;
 use yii\helpers\Url;
 
 class BaseBackendController extends BaseController {
@@ -22,25 +20,4 @@ class BaseBackendController extends BaseController {
         }
         return parent::beforeAction($action);
     }
-
-    protected function userActivity($actionId = array(),$descrition='') {
-        $currentUser = CommonHelper::getUser();
-      
-        foreach ($actionId as $value){
-           $actionLog = new ActionLog();
-           $actionLog->action_type=$value;
-           $actionLog->date=date('Y-m-d');
-           $actionLog->time=date('H:m:s');
-           if($descrition != ''){
-           $actionLog->description=$descrition;
-           }else{
-           $actionLog->description=$value;
-           }
-           $actionLog->user=$currentUser['id'];
-           if($actionLog->validate()){
-               $actionLog->save(false);
-           }
-        }
-    }
-
 }
--- common/controllers/BaseController.php
+++ common/controllers/BaseController.php
@@ -3,7 +3,8 @@ namespace common\controllers;
 
 use Yii;
 use yii\web\Controller;
-
+use common\helpers\CommonHelper;
+use common\models\ActionLog;
 /**
  * Base controller
  */
@@ -14,4 +15,24 @@ class BaseController extends Controller
 
 		parent::__construct($id, $module, $config);	
 	}
+        
+        protected function userActivity($actionId = '',$descrition='') {
+        $currentUser = CommonHelper::getUser();
+
+           $actionLog = new ActionLog();
+           $actionLog->action_type=$actionId;
+           $actionLog->date=date('Y-m-d');
+           $actionLog->time=date('H:m:s');
+           if($descrition != ''){
+           $actionLog->description=$descrition;
+           }else{
+             $actionIds=  explode('_', $actionId);
+             $descrition= implode(' ', $actionIds);
+             $actionLog->description=$descrition;
+           }
+           $actionLog->user=$currentUser['id'];
+           if($actionLog->validate()){
+               $actionLog->save(false);
+           } 
+    }
 }
