a29d0eb972767c2c37fa4c46875d1d7f71a1a942
Hardik Devariya <hardik.devariya@sparsh1.com>
2018-07-11 10:58:22 +0530

    Added favicon

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -21,6 +21,7 @@ $adminUrl = CommonHelper::getPath('admin_url');
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="<?php echo CommonHelper::getPath('admin_url'); ?>images/favicon.png" />
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">  
