aad69887332ff2565d7b819063c0d75b200ee318
Garima Pandey <garima.pandey@sparsh1.com>
2018-07-18 17:00:49 +0530

    dropdown truncate resolved in market admin side

--- backend/views/market/index.php
+++ backend/views/market/index.php
@@ -26,10 +26,10 @@ $formUrl = Url::to(['market/index']);
                         <?= Html::beginForm($formUrl, 'get', ['data-pjax' => '', 'id' => 'search-users']); ?>
                         <div class="filter-search dataTables_filter clearfix">
                             <div class="row">
-                                <div class="col-md-8">
+                                <div class="col-md-7">
                                     <?php echo Html::input('text', 'search', isset($filters['search']) ? $filters['search'] : '', ['class' => 'form-control','placeholder'=>'Search','id' => 'user-text'])?> 
                                 </div>
-                                 <div class="col-md-4">
+                                 <div class="col-md-5">
                                     <?= Html::dropDownList('limit', isset($filters['limit']) ? $filters['limit'] : '' ,Yii::$app->params['limit'],  ['class' => 'form-control','id' => 'user-limit']) ?>
                                 </div>
                             </div>
